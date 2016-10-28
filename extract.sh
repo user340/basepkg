fa4567e7ad5a852067624dcb7d21e6feb60013b9
uki <mail@e-yuuki.org>
2016-10-29 03:06:22 +0900

    save today work

---
+++ extract.sh
@@ -0,0 +1,6 @@
+. "./subroutine"
+for i in base comp etc games kern-GENERIC man misc modules tests text
+do
+	test -d ./work/$i || mkdir ./work/$i
+	tar zxf "$sets"/$i.tgz -C ./work/$i
+done
--- make_basepkg.sh
+++ make_basepkg.sh
@@ -1,7 +1,9 @@
-clean_plus_file $pkgname
+. "./subroutine"
+pkgname="$(echo $1 | sed 's/\///')"
+# clean_plus_file $pkgname
 make_list $pkgname
-make_BUILD_INFO $pkgname
-make_COMMENT $pkgname
-make_CONTENTS $pkgname
-make_DESC $pkgname
-make_PKG $pkgname
+#make_BUILD_INFO $pkgname
+#make_COMMENT $pkgname
+#make_CONTENTS $pkgname
+#make_DESC $pkgname
+#make_PKG $pkgname
