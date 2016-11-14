c2ea0a471b343a3fc4ba0f61789e80f191549e88
uki <mail@e-yuuki.org>
2016-11-15 02:57:18 +0900

    split base and comp packages

--- make_basepkg.sh
+++ make_basepkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . "./subroutine"
 pkgname="$(echo $1 | sed 's/\///')"
- clean_plus_file $pkgname
+clean_plus_file $pkgname
 make_list $pkgname
 make_BUILD_INFO $pkgname
 make_COMMENT $pkgname
