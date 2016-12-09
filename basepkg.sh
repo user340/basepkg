18c39cad840114e302e2e6d943f3274dfbd125ce
Yuuki Enomoto <mail@e-yuuki.org>
2016-12-10 01:55:53 +0900

    divisioning packages

--- basepkg.sh
+++ basepkg.sh
@@ -4,6 +4,7 @@
 usage() {
 	echo "usage: ./basepkg.sh operation"
 	echo " Create packages operations"
+	echo "    pkg        create packages"
 	echo ""
 	echo " Other operations"
 	echo "    baseroot   create base root hierachy"
@@ -20,5 +21,11 @@ case $1 in
 	baseroot) make_dirtree > ./sets/base/root/root.list ;;
 	clean) clean_plus_file ;;
 	extract) extract_base ;;
+	pkg)     make_BUILD_INFO base/openssl
+	         make_COMMENT base/openssl
+					 make_CONTENTS base/openssl
+					 make_DESC base/openssl
+					 make_PKG base/openssl
+	         ;;
 	*) usage ;;
 esac
