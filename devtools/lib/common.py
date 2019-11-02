4dc9038df5ed70cc3014516031688f03430e6a89
Yuuki Enomoto <uki@e-yuuki.org>
2019-11-02 13:12:31 +0900

    Export common information into lib/common.py

---
+++ devtools/lib/common.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+NetBSD_sets = frozenset(
+    [
+        'base',
+        'comp',
+        'debug',
+        'etc',
+        'games',
+        'man',
+        'misc',
+        'modules',
+        'rescue',
+        'tests',
+        'text',
+        'xbase',
+        'xcomp',
+        'xdebug',
+        'xetc',
+        'xfont',
+        'xserver'
+    ]
+)
