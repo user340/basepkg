c392b74e8147fd875c9167c571eb66f31256b84a
Yuuki Enomoto <uki@e-yuuki.org>
2020-01-11 23:31:14 +0900

    tested filtering awk script

--- tests/obsolete_filter_test.sh
+++ tests/obsolete_filter_test.sh
@@ -3,8 +3,17 @@
 # shellcheck disable=SC1091
 # shellcheck disable=SC2039
 
-awk_script="../lib/awk/obsolete_filter.awk"
-run_awk="awk -v category=test -v moduledir=x -v machine=amd64 -v release_k=9.99.30 -f $awk_script"
+run_awk()
+{
+    local script="../lib/awk/obsolete_filter.awk"
+
+    awk \
+        -v category=test \
+        -v moduledir=module \
+        -v machine=amd64 \
+        -v release_k=9.99.30 \
+        -f $script
+}
 
 test_given_obsolete_package_pattern()
 {
@@ -12,9 +21,36 @@ test_given_obsolete_package_pattern()
     local expected=""
     local result
 
-    result="$(echo "$testcase" | $run_awk)"
+    result="$(echo "$testcase" | run_awk)"
 
-    assertEquals "$result" "$expected"
+    assertEquals "$expected" "$result"
+}
+
+test_given_obsolete_labeled_package_pattern()
+{
+    local testcase="xxx	test-testing-package	obsolete"
+    local expected=""
+    local result
+
+    result="$(echo "$testcase" | run_awk)"
+
+    assertEquals "$expected" "$result"
+}
+
+test_valid_pattern()
+{
+    local testcase="
+# comment
+./@MODULEDIR@/aio	test-kernel-modules	kmod
+./stand/@MACHINE@/@OSRELEASE@	test-kernel-modules	kmod
+./stand/@MACHINE@/@OSRELEASE@	xxxx-kernel-modules	kmod"
+    local expected="module/aio test-kernel-modules kmod
+stand/amd64/9.99.30 test-kernel-modules kmod"
+    local result
+
+    result="$(echo "$testcase" | run_awk)"
+
+    assertEquals "$expected" "$result"
 }
 
 . ./common.sh
